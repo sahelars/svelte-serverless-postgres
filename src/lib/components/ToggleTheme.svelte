2dbe052713c799a9e3d3aba86bddb1acc0fe2d58
Sam Larsen <sam@slarsen.io>
2025-05-14 22:13:29 -0400

    Improve toggle theme

--- src/lib/components/ToggleTheme.svelte
+++ src/lib/components/ToggleTheme.svelte
@@ -1,7 +1,9 @@
 <script>
 	let { classname, size = '20px' } = $props();
 
-	let theme = $state('light');
+	const defaultTheme = 'dark';
+	let theme = $state(defaultTheme);
+	let isRotated = $state(false);
 
 	function getCookie(name) {
 		const value = `; ${document.cookie}`;
@@ -17,12 +19,13 @@
 	$effect(() => {
 		if (typeof window !== 'undefined') {
 			const savedTheme = getCookie('theme');
-			theme = savedTheme || 'light';
+			theme = savedTheme || defaultTheme;
 			document.documentElement.setAttribute('data-theme', theme);
 		}
 	});
 
 	function toggleTheme() {
+		isRotated = !isRotated;
 		theme = theme === 'light' ? 'dark' : 'light';
 		document.documentElement.setAttribute('data-theme', theme);
 		setCookie('theme', theme);
@@ -35,29 +38,8 @@
 	aria-label={`Switch to ${theme === 'light' ? 'dark' : 'light'} theme`}
 >
 	<div
-		class="ease-out-cubic-bezier col-start-1 row-start-1 transition-all duration-200"
-		class:opacity-0={theme !== 'light'}
-		class:rotate-45={theme !== 'light'}
-		class:scale-0={theme !== 'light'}
-	>
-		<svg
-			stroke-width="0"
-			viewBox="0 0 24 24"
-			height={size}
-			width={size}
-			xmlns="http://www.w3.org/2000/svg"
-			><path d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8V16Z"></path><path
-				fill-rule="evenodd"
-				clip-rule="evenodd"
-				d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2ZM12 4V8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16V20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4Z"
-			></path></svg
-		>
-	</div>
-	<div
-		class="ease-out-cubic-bezier col-start-1 row-start-1 transition-all duration-200"
-		class:opacity-0={theme !== 'dark'}
-		class:rotate-45={theme !== 'dark'}
-		class:scale-0={theme !== 'dark'}
+		class="ease-out-cubic-bezier col-start-1 row-start-1 transition-all duration-300"
+		class:rotate-360={isRotated}
 	>
 		<svg
 			stroke-width="0"
